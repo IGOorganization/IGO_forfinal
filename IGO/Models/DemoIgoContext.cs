02e8042c4d6af89f8ad386c9c73a435c3c56bee6
summershin <102308224+summershin@users.noreply.github.com>
2022-07-27 12:31:43 +0800

    子慧第一次合併，後續調整完成

--- IGO/Models/DemoIgoContext.cs
+++ IGO/Models/DemoIgoContext.cs
@@ -51,14 +51,14 @@ namespace IGO.Models
         public virtual DbSet<TTicketType> TTicketTypes { get; set; }
         public virtual DbSet<TVoucher> TVouchers { get; set; }
 
-//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//        {
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=DemoIgo;Integrated Security=True;");
-//            }
-//        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=DemoIgo;Integrated Security=True");
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -146,7 +146,6 @@ namespace IGO.Models
 
             modelBuilder.Entity<TCollectionGroupDetail>(entity =>
             {
-
                 entity.HasKey(e => e.FCollectionGroupDetailId);
 
                 entity.ToTable("tCollectionGroupDetail");
@@ -156,7 +155,6 @@ namespace IGO.Models
                 entity.Property(e => e.FCollectionGroupId).HasColumnName("fCollectionGroupID");
 
                 entity.Property(e => e.FCollectionId).HasColumnName("fCollectionID");
-
 
                 entity.HasOne(d => d.FCollectionGroup)
                     .WithMany(p => p.TCollectionGroupDetails)
